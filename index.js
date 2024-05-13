8f450e23d52e58985d758df18a9e8e82ed621d6e
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-05-13 09:27:21 -0700

    booking get data  modified

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const verifyToken = async (req, res, next) => {
 
 
 
-const uri = "mongodb+srv://ih9066588:Y6CV5W4ex17IK7p2@cluster0.c25yoec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c25yoec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
